42bdfc9628094449d35232259abb7cea283117a6
omarJerbi <omarjerbi98@gmail.com>
2023-05-01 12:30:14 +0200

    fav OK

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ export const metadata = {
     title: 'OJXwebdev | Sviluppiamo il TUO sito web',
     description: 'Affidandoti ad OJX trasformeremo le tue idee nel tuo sito web! Sviluppo di siti internet per eventi, aziende, eCommerce, blogs, portfolio personali e molti altri.',
     icons: {
-        icon: "/_next/static/media/favicon.1d647803.png",
-        shortcut: "/_next/static/media/favicon.1d647803.png",
+        icon: "/_next/static/media/favicon.f7bb4033.png",
+        shortcut: "/_next/static/media/favicon.f7bb4033.png",
     },
     keywords: ['ojxwebdev', 'sviluppo web', "siti web", "ecommerce", "blog", "programmazione", "business", "sito web"],
     authors: [{ name: "OmarJ", url: "https://github.com/Omar-jerbi" }],
